7999ed475e877ba1d3f504124a0173cc3be139f5
Ben Walton <bdwalton@gmail.com>
2023-07-05 21:21:37 +0100

    Add code to convert program code to instructions
    This code takes assembly code for baby and converts it into
    instructions. We've added some test coverage for it as well.

--- baby.go
+++ baby.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 const (
@@ -22,9 +24,35 @@ const (
 	STP         // Stop (111)
 )
 
+var opNames = []string{"JMP", "SUB", "LDN", "CMP", "JRP", "SUB", "STO", "STP"}
+var nameOps = map[string]int32{
+	"JMP": JMP,
+	"SUB": SUB,
+	"LDN": LDN,
+	"CMP": CMP,
+	"JRP": JRP,
+	// SUB2
+	"STO": STO,
+	"STP": STP,
+}
+
 type instruction struct {
 	op   int32
 	data int32
+}
+
+func (i *instruction) String() string {
+	var sb strings.Builder
+
+	sb.WriteString(opNames[i.op])
+
+	switch i.op {
+	case CMP, STP:
+	default:
+		sb.WriteString(fmt.Sprintf(" %d", i.data))
+	}
+
+	return sb.String()
 }
 
 func instFromWord(word int32) *instruction {
@@ -34,12 +62,6 @@ func instFromWord(word int32) *instruction {
 	}
 
 	return i
-}
-
-var names = []string{"JMP", "SUB", "LDN", "CMP", "JRP", "SUB", "STO", "STP"}
-
-func (i *instruction) String() string {
-	return names[i.op]
 }
 
 type register int32
@@ -74,6 +96,41 @@ func (b *baby) Run() {
 		}
 
 		b.Step()
+	}
+}
+
+var (
+	missingOp      = errors.New("invalid code - missing operand")
+	extraOp        = errors.New("invalid code - unexpected argument")
+	badOperand     = errors.New("invalid code - invalid operand")
+	badInstruction = errors.New("invalid code - unknown instruction")
+)
+
+func instructionFromCode(code string) (*instruction, error) {
+	parts := strings.Split(code, " ")
+
+	switch parts[0] {
+	case "CMP", "STP":
+		if len(parts) > 1 {
+			return nil, extraOp
+		}
+		return &instruction{op: nameOps[parts[0]]}, nil
+	default:
+		if len(parts) < 2 {
+			return nil, missingOp
+		}
+
+		operand, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return nil, badOperand
+		}
+
+		op, ok := nameOps[parts[0]]
+		if !ok {
+			return nil, badInstruction
+		}
+
+		return &instruction{op: op, data: int32(operand)}, nil
 	}
 }
 
---
+++ baby_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestInstructionFromCode(t *testing.T) {
+	cases := []struct {
+		input   string
+		want    *instruction
+		wantErr error
+	}{
+		// Good
+		{"JMP 22", &instruction{op: JMP, data: 22}, nil},
+		{"SUB 21", &instruction{op: SUB, data: 21}, nil},
+		{"LDN 21", &instruction{op: LDN, data: 21}, nil},
+		{"CMP", &instruction{op: CMP}, nil},
+		{"JRP 10", &instruction{op: JRP, data: 10}, nil},
+		{"STO 2", &instruction{op: STO, data: 2}, nil},
+		{"STP", &instruction{op: STP}, nil},
+
+		// Bad
+		{"JMP", nil, missingOp},
+		{"SUB", nil, missingOp},
+		{"LDN", nil, missingOp},
+		{"CMP 21", nil, extraOp},
+		{"JRP", nil, missingOp},
+		{"STO", nil, missingOp},
+		{"STP 21", nil, extraOp},
+
+		// Ugly
+		{"BAD 21", nil, badInstruction},
+		{"21 BAD", nil, badOperand},
+		{"21 21", nil, badInstruction},
+	}
+
+	for i, tc := range cases {
+		got, err := instructionFromCode(tc.input)
+		if !reflect.DeepEqual(got, tc.want) || err != tc.wantErr {
+			t.Errorf("case %d: got(%v) != want(%v) || err(%v) != wantErr(%v)", i, got, tc.want, err, tc.wantErr)
+		}
+	}
+}
